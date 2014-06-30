2ea8d6cd82d122205c89a1d5c05d109bb01dc0dc
SNSD1488 <shrudc@gmail.com>
2014-06-30 16:38:39 -0700

    Formatting changes. Multiline messages. New jokes.

--- bot.js
+++ bot.js
@@ -1,63 +1,73 @@
 API.setVolume(0);
 
-var bot =
-{
-    pointBreakdownCommand: function(chatObj)
-    {
+var bot = {
+    pointBreakdownCommand: function (chatObj) {
         var self = this,
-            user = API.getUser(chatObj.fromID);
+            user = API.getUser(chatObj.fromID),
+            totalPoints = user.curatorPoints + user.djPoints + user.listenerPoints;
 
-        if (chatObj.message == "!points") {
-            var totalPoints = user.curatorPoints + user.djPoints + user.listenerPoints;
-
+        if (chatObj.message === "!points") {
             self.sendChat(
                 "Your points breakdown is: " +
-                "Curates: "   + user.curatorPoints  + " (" + (user.curatorPoints /totalPoints*100).toFixed(1) + "%) " +
-                "DJing: "     + user.djPoints       + " (" + (user.djPoints      /totalPoints*100).toFixed(1) + "%) " +
-                "Listening: " + user.listenerPoints + " (" + (user.listenerPoints/totalPoints*100).toFixed(1) + "%)",
+                    "Curates: "   + user.curatorPoints  + " (" + (user.curatorPoints  / totalPoints * 100).toFixed(1) + "%) " +
+                    "DJing: "     + user.djPoints       + " (" + (user.djPoints       / totalPoints * 100).toFixed(1) + "%) " +
+                    "Listening: " + user.listenerPoints + " (" + (user.listenerPoints / totalPoints * 100).toFixed(1) + "%)",
                 user
             );
         }
     },
 
-    sendSongStartMessage: function()
+    sendSongStartMessage: function ()
     {
         var self = this,
             user = API.getDJ(),
             song = API.getMedia(),
-            messages = new Array(
-                "here we go rock out",
-                "here we go guys",
-                "here is a good video",
-                "here is a great song",
-                "here we go everyone",
-                "hell yeahh"
-            ),
-            messagesRare = new Array(
-                "saddle up, pardner, you are about to hear a good song",
-                "to be perfectly honest, i'm not into this next one all that much",
-                "fucking piece of my dick.....",
-                "listen, i know the start of this track is gay, but bear with me, just wait for the drop, it's worth it",
-                "in a way, {{artist}}s songs are all sad, because they all have an ending",
-                "thanks for 1mil views! {{artist}} ftw",
-                "omgomgom i cna't beliee ppl are liking this song",
-                "{{artist}} are alright..... but not as good as type o negative",
-                "{{artist}} were such a great band, r.i.p. kurt kobain >:",
-                "now playing: {{artist}} - {{title}} ({{dubstep}} remix)",
-                "a steampunk fantasy based on the new {{artist}} album",
-                "look here you dumb slut. don't you ever say {{artist}} is for fags and losers. {{artist}} is legendary. they created and change what we call music.",
-                "{{artist}} is for fags, the beatles arn't for fags!!! got it!!!",
-                "I feel like too many people these days only take {{artist}} for face value, only know their radio hits, and just haven't dug into this band nearly as much as I have."
-            ),
-            messagesNDAD = new Array(
-                "wow hydropolis who's this exciting new band I don't think you've ever played them before??"//,
-                //"dang, you're always bringing out new and exiciting artists that you've never played",
-                //"i've never heard anything like this, why haven't you played this band here before"
-            );
+            messages = [
+                    "here we go rock out",
+                    "here we go guys",
+                    "here is a good video",
+                    "here is a great song",
+                    "here we go everyone",
+                    "hell yeahh",
+                ],
+            messagesUncommon = [
+                    "GREAT SONG",
+                    "you guys always ROCK﻿",
+                    "I LOVE YOUR MUSIC ,,,,,,,,,I WAS IN MY 20S .....WHEN I STAYED UP ALNIGHT ---))﻿",
+                    "AWESOME SONG",
+                    "motley crue﻿",
+                    "HERE WE GO SOME DR FEEL GOOD!!!!!.....LETS ROCK !",
+                    "HERE WE GO !!!..GOTTA LOVE PARTYS",
+                ],
+            messagesRare = [
+                    "saddle up, pardner, you are about to hear a good song",
+                    "to be perfectly honest, i'm not into this next one all that much",
+                    "fucking piece of my dick.....",
+                    "listen, i know the start of this track is gay, but bear with me, just wait for the drop, it's worth it",
+                    "in a way, {{artist_lower}}s songs are all sad, because they all have an ending",
+                    "thanks for 1mil views! {{artist_lower}} ftw",
+                    "omgomgom i cna't beliee ppl are liking this song",
+                    "{{artist_lower}} are alright..... but not as good as type o negative",
+                    "{{artist_lower}} were such a great band, r.i.p. kurt kobain >:",
+                    "Now Playing: {{artist}} - {{title}} ({{dubstep}} remix)",
+                    "a steampunk fantasy based on the new {{artist_lower}} album",
+                    "look here you dumb slut. don't you ever say {{artist_lower}} is for fags and losers. {{artist_lower}} is legendary. they created and change what we call music.",
+                    "{{artist_lower}} is for fags, the beatles arn't for fags!!! got it!!!",
+                    "I feel like too many people these days only take {{artist}} for face value, only know their radio hits, and just haven't dug into this band nearly as much as I have.",
+                    "Come To {{country}}!!!!!!",
+                    [ "I'm on the phone wit the CEO of iTunes he's telling me {{artist_lower}} #{{title_no_spaces}} only been on iTunes for 1hour and u selling like crazy my staff is telling me the server is about to shut down", "I gave the ok to add the most powerful servers to handle this #{{title_no_spaces}} congrats looks like more plaks for U {{artist_lower}} #sufferingfromsuccess" ],
+                ],
+            messagesNDAD = [
+                    "wow hydropolis who's this exciting new band I don't think you've ever played them before??",
+                    "i've never heard anything like this, why haven't you played this band here before",
+                ];
 
-        // 20% chance to use rare messages
-        if (Math.random() < 0.1)
+
+        if (Math.random() < 1/10) { //       10% chance to use uncommon messages (increase when we have more messages)
+            messages = messagesUncommon;
+        } else if (Math.random() < 1/9) { // 10% chance to use rare messages
             messages = messagesRare;
+        }
 
         //own Hydropolis
         var artistCheck = new RegExp("/nero's day at disneyland|neros day at disneyland|bousfield|ndad");
@@ -69,7 +79,7 @@ var bot =
         self.sendChat(self.getRandomArrayValue(messages));
     },
 
-    removeFromWaitlistIfVoteWasMeh: function(vote)
+    removeFromWaitlistIfVoteWasMeh: function (vote)
     {
         var self = this,
             message = "Meh-ing is frowned upon here. If you think a song is gay, please do as the gay community would do, and be positive. Thanks.";
@@ -86,76 +96,135 @@ var bot =
         }
     },
 
-    sendUserJoinMessage: function(user)
+    sendUserJoinMessage: function (user)
     {
         var self = this;
 
         self.sendChat("Welcome to " + self.getRoomName(), user);
     },
 
-    getRoomName: function()
+    getRoomName: function ()
     {
         var self = this,
-            roomnames = new Array(
-            "★Anime Games Music★",
-            "★Brazil Eletro Music ★",
-            "██►Dubstep For Your Nipples◄██",
-            "Drum & Bass (NoDrumstep)",
-            "ϟ Electro,Dυbรтєρ & Techno ϟ",
-            "FiM: Your daily ponies",
-            "Monstercat + Tasty = #Tastycat",
-            "/r/music",
-            "Rock Wins",
-            "♪ϟ☆ Self-Help Audiobooks Den ☆ϟ♪",
-            "高登音樂台",
-            "{{artist}} 24/7",
-            "{{dubstep}} 24/7"
-        );
+            roomnames = [
+                    "★Anime Games Music★",
+                    "███►Anime Music Music {{country}}◄███",
+                    "★Brazil Eletro Music ★",
+                    "♪ K-Pop Fans {{country}} ♪",
+                    "██►Dubstep For Your Nipples◄██",
+                    "Deathcore / Metalcore / Hardcore",
+                    "Drum & Bass (NoDrumstep)",
+                    "Electro {{country}}",
+                    "ϟ Electro, Dυbรтєρ & Techno ϟ",
+                    "FiM: Your daily ponies",
+                    "HilterとAIDS",
+                    "I ♥ the 90's and 00's",
+                    "Just a chill room...",
+                    "Monstercat + Tasty = #Tastycat",
+                    "♫Music Nation™♫ #AnyGenre!",
+                    "♥ ☆ Nightcore ☆ ♥",
+                    "/r/music",
+                    "Rock, Animes & Dubstep",
+                    "Rock Wins",
+                    "♪ϟ☆ Self-Help Audiobooks Den ☆ϟ♪",
+                    "THE HATALLUMINATI",
+                    "[T]rance [H]ouse [C]hill",
+                    "Video Game Music [+--oo]",
+                    "高登音樂台",
+                    "{{artist}} 24/7",
+                    "{{artist}}VEVO",
+                    "★{{country}} EDM Community★",
+                    "{{dubstep}} 24/7",
+                ];
 
-        // 5% chance to use funny joke room name
-        if (Math.random() < 0.1)
+        if (Math.random() < 1/10) // 10% chance to use funny joke room name
             return self.getRandomArrayValue(roomnames);
         else
             return "Hitler & The AIDS, Live!!";
     },
 
-    sendChat: function(message, user)
+    sendChat: function (messages, user)
     {
         var self = this;
 
-        if (typeof user === "object")
-            message = "@" + user.username + " " + message;
+        if (!self.isArray(messages)) {
+            messages = [ messages ];
+        }
 
-        message = self.sparseTeenMustache(message);
+        messages.forEach(function(message) {
+            if (typeof user === "object") {
+                message = "@" + user.username + " " + message;
+            }
 
-        API.sendChat(message);
+            message = self.sparseTeenMustache(message);
+
+            API.sendChat(message);
+        });
     },
 
-    sparseTeenMustache: function(text) //templating the hard way
+    isArray: function (someVar)
+    {
+        return (Object.prototype.toString.call( someVar ) === '[object Array]');
+    },
+
+    sparseTeenMustache: function (text) //templating the hard way
     {
         var self = this,
             song = API.getMedia(),
-            dubstep = new Array(
-                "borgore",
-                "datsik",
-                "deadmau5",
-                "excision",
-                "flux pavilion",
-                "knife party",
-                "nero",
-                "skream",
-                "skrillex",
-                "zomboy"
-            );
+            countries = [
+                    "Argentina",
+                    "Bosnia",
+                    "Brasil",
+                    "Brazil",
+                    "Bulgaria",
+                    "Cambodia",
+                    "Chile",
+                    "Colombia",
+                    "Croatia",
+                    "Indonesia",
+                    "Kosovo",
+                    "Latvia",
+                    "Malaysia",
+                    "North Korea",
+                    "Paraguay",
+                    "Peru",
+                    "Philippines",
+                    "Romania",
+                    "Serbia",
+                    "Slovakia",
+                    "Turkey",
+                    "Venezuela",
+                    "Vietnam",
+                ],
+            dubstep = [
+                    "Borgore",
+                    "Datsik",
+                    "Deadmau5",
+                    "Excision",
+                    "Flux Pavilion",
+                    "Knife Party",
+                    "Nero",
+                    "Owl City",
+                    "Skream",
+                    "Skrillex",
+                    "ϟƘƦƖןןΣx﻿",
+                    "Zomboy",
+                ];
 
         return text
-            .replace(/{{artist}}/g, song.author.toLowerCase())
-            .replace(/{{title}}/g, song.title.toLowerCase())
+            .replace(/{{artist}}/g,           song.author)
+            .replace(/{{artist_lower}}/g,     song.author.toLowerCase())
+            .replace(/{{artist_no_spaces}}/g, song.author.toLowerCase().replace(/ /g, ""))
+            .replace(/{{country}}/g, self.getRandomArrayValue(countries))
             .replace(/{{dubstep}}/g, self.getRandomArrayValue(dubstep))
+            .replace(/{{dubstep_lower}}/g, self.getRandomArrayValue(dubstep).toLowerCase())
+            .replace(/{{title}}/g,           song.title)
+            .replace(/{{title_lower}}/g,     song.title.toLowerCase())
+            .replace(/{{title_no_spaces}}/g, song.title.toLowerCase().replace(/ /g, ""))
         ;
     },
 
-    getRandomArrayValue: function(array)
+    getRandomArrayValue: function (array)
     {
         var randomIndex = Math.floor(Math.random()*array.length);
         return array[randomIndex];
